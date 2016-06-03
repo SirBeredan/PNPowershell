889a1f50b19f3a28fcf4c2828d78e30733849456
Brendan Barton <Brendan.Barton@education.wa.edu.au>
2016-06-03 11:25:19 +0800

    Initail commit

---
+++ Get-PNPUtilList.ps1
@@ -0,0 +1,37 @@
+﻿<#
+.Synopsis
+   Retreives list of installed drivers
+.Description
+   Parses PNPutil in to an object that can then be used to manipulate drivers.
+.EXAMPLE
+   Get-PNPutilList | where {$_.Class -eq "Printers"} 
+.INPUTS
+   None
+.OUTPUTS
+   Creates object of All 3rd Party PNP Drivers installed in DriveStore
+.NOTES
+   Requires Windows Vista and Above to use PNPutil.exe
+#>
+Function Get-PNPutilList
+{
+    $List = (pnputil -e) | select -Skip 2
+    $Props = @{'Name'="";
+    'Provider'="";
+    'Class'="";
+    'Date'="";
+    'Signer'=""
+    }
+    $PNPObject = New-Object -TypeName System.Collections.ArrayList
+
+    for ($Count = 0; $Count -lt $List.Count; $Count = $Count + 6)
+    {
+        $DriverStore = New-Object PSCustomObject -Property $Props
+        $DriverStore.Name=$List[$Count].Split(':')[1].trim()
+        $DriverStore.Provider=$List[$Count+1].Split(':')[1].trim()
+        $DriverStore.Class=$List[$Count+2].Split(':')[1].trim()
+        $DriverStore.Date=$list[$Count+3].Split(':')[1].trim()
+        $DriverStore.Signer=$list[$Count+4].Split(':')[1].trim()
+        $PNPObject.add($DriverStore) | Out-Null
+    }
+    return $PNPObject
+}
---
+++ Remove-PNPPrintDrivers.ps1
@@ -0,0 +1,27 @@
+﻿<#
+.Synopsis
+   Removes all WHQL Signed 3rd party Print Drivers
+.Description
+   Uses Get-PNPUtilList.ps1 to create a PNPObject which then deletes all installed 3rd Party Print Drivers.
+.EXAMPLE
+  Remove-PNPPrintDrivers.ps1
+.NOTES
+   Requires Windows Vista and Above to use PNPutil.exe
+#>
+. .\Get-PNPutilList.ps1
+$PNPObject = Get-PNPutilList | where {$_.Class -eq "Printers"} | where {$_.Signer -eq "Microsoft Windows Hardware Compatibility Publisher"}
+foreach ($PNPitem in $PNPObject)
+{
+    $DriverName = $PNPItem.name
+    pnputil -d $DriverName
+}
+Restart-Service -Name Spooler
+$Service = Get-Service -Name Spooler
+If (($Service.status) -eq "Running")
+{
+    write-host "Print Spool Is Running" -ForegroundColor Green
+}
+else
+{
+    write-host "Print Spooler Is Not Running" -ForegroundColor Red
+}
